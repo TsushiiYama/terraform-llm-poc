93183c9baf68c17089f5b6abfbcaa3f243afa19b
Naim <naim.memsout@gmail.com>
2025-05-15 12:03:56 +0000

    Not much

--- shared/main.tf
+++ shared/main.tf
@@ -1,12 +1,27 @@
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
+resource "docker_image" "wordpress" {
+ name = "wordpress:latest"
 }
 
-resource "docker_container" "nginx" {
-  name  = "nginx-container"
-  image = docker_image.nginx.name
-  ports {
-    internal = 80
-    external = 8080
-  }
+resource "docker_container" "wordpress" {
+ name  = "wordpress-container"
+ image = docker_image.wordpress.name
+ ports {
+ internal = 80
+ external = 8080
+ }
+ depends_on = [docker_container.mysql]
+}
+
+resource "docker_image" "mysql" {
+ name = "mysql:latest"
+}
+
+resource "docker_container" "mysql" {
+ name  = "mysql-container"
+ image = docker_image.mysql.name
+ environment = ["MYSQL_ROOT_PASSWORD=rootpassword"]
+ ports {
+ internal = 3306
+ external = 3306
+ }
 }
