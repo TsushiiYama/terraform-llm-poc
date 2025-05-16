1c786eb8ccf6112a3ede82a9311c2479222d3745
root <root@VM-NAIM.bggpdjeyvtrefjogrznjffooxb.ax.internal.cloudapp.net>
2025-05-16 08:26:31 +0000

    Syncing commit

--- shared/main.tf
+++ shared/main.tf
@@ -1,27 +1,47 @@
-resource "docker_image" "wordpress" {
- name = "wordpress:latest"
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
 }
 
-resource "docker_container" "wordpress" {
- name  = "wordpress-container"
- image = docker_image.wordpress.name
- ports {
- internal = 80
- external = 8080
- }
- depends_on = [docker_container.mysql]
+resource "docker_container" "nginx" {
+  name  = "nginx-container"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+  env = [
+    "VARIABLE_NAME=value"
+  ]
 }
 
-resource "docker_image" "mysql" {
- name = "mysql:latest"
+resource "docker_image" "nodejs" {
+  name = "node:latest"
 }
 
-resource "docker_container" "mysql" {
- name  = "mysql-container"
- image = docker_image.mysql.name
- environment = ["MYSQL_ROOT_PASSWORD=rootpassword"]
- ports {
- internal = 3306
- external = 3306
- }
+resource "docker_container" "nodejs" {
+  name  = "nodejs-container"
+  image = docker_image.nodejs.name
+  ports {
+    internal = 3000
+    external = 3000
+  }
+  env = [
+    "VARIABLE_NAME=value"
+  ]
+}
+
+resource "docker_image" "mongodb" {
+  name = "mongo:latest"
+}
+
+resource "docker_container" "mongodb" {
+  name  = "mongodb-container"
+  image = docker_image.mongodb.name
+  ports {
+    internal = 27017
+    external = 27017
+  }
+  env = [
+    "VARIABLE_NAME=value"
+  ]
 }
