6621c38e9a5175028ced2cb14935386f920c96b5
TsushiiYama <naim.memsout@gmail.com>
2025-05-19 15:36:08 +0200

    Changed LLM

--- api/ollama_service.py
+++ api/ollama_service.py
@@ -88,7 +88,7 @@ class ITerraformService(ABC):
 
 # Concrete Service Implementations
 class OllamaLLMService(ILLMService):
-    def __init__(self, model_name: str = "codegemma:2b"):
+    def __init__(self, model_name: str = "codellama"):
         self.model_name = model_name
     
     def generate_code(self, prompt: str) -> str:
